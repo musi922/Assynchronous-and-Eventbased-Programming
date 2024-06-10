000b35462438e5f758a19690a7bb6d6f4d3e25ce
Richard <richardmusime6@gmail.com>
2024-06-10 12:36:05 +0200

    the functions showing the way synchronous and asynchronous differ

---
+++ syncandasync.js
@@ -0,0 +1,43 @@
+//synchronous
+function task1(){
+    console.log("task1 completed");
+}
+function task2(){
+    console.log("task2 completed");
+}
+function task3(){
+    console.log("task3 completed");
+}
+function task4(){
+    console.log("task4 completed");
+}
+
+task1()
+task2()
+task3()
+task4()
+console.log("All done");
+//asynchronous
+function task1(){
+    setTimeout(()=>{
+        console.log("task1 completed");},2000)
+}
+function task2(){
+    setTimeout(()=>{
+        console.log("task2 completed");},1000)
+}
+function task3(){
+    setTimeout(()=>{
+        console.log("task3 completed");},4000)
+}
+function task4(){
+    setTimeout(()=>{
+        console.log("task4 completed");
+    },3000)
+}
+task1()
+task2()
+task3()
+task4()
+
+
