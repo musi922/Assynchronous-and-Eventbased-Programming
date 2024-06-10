72f46862bff6819f831f516d133eb7e9df475a16
Richard <richardmusime6@gmail.com>
2024-06-10 12:26:09 +0200

    Learned the way we use method chaining and all its methods

--- classes.js
+++ classes.js
@@ -1,39 +1,68 @@
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;  
+//     }
+
+//     static fullname() {
+//         return `Richard 20`;
+//     }
+
+//     get totalAge() {
+//         return `${this.age}`;
+//     }
+
+//     get studentAge() {
+//         return this.age;
+//     }
+// }
+
+// class Student extends Person {
+//     constructor(name, age, studentLastName) {
+//         super(name, age);  
+//         this.studentLastName = studentLastName;
+//     }
+
+//     static fullDetails() {
+//         return `Student Richard Musime, Age: 40`;
+//     }
+// }
+
+
+// var person1 = new Person("Richard", 20);
+// console.log(Person.fullname());
+// console.log(person1.totalAge);
+
+// var student1 = new Student("Richard", 40, "Musime");
+// console.log(Student.fullDetails());
+// console.log(student1.studentAge);
+// console.log(student1.studentLastName);
+
+//How method chaining works
+
 class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;  
     }
 
-    static fullname() {
-        return `Richard 20`;
+    firstname(name) {
+        this.name = name
+        return this;
     }
 
-    get totalAge() {
-        return `${this.age}`;
+    totalAge(age) {
+        this.age = age
+        return this;
     }
-
-    get studentAge() {
-        return this.age;
+    fullname(){
+        return `Musime ${this.name}`
     }
-}
-
-class Student extends Person {
-    constructor(name, age, studentLastName) {
-        super(name, age);  
-        this.studentLastName = studentLastName;
-    }
-
-    static fullDetails() {
-        return `Student Richard Musime, Age: 40`;
+    fullnames(){
+        return `Musime ${this.name}`
     }
 }
 
 
-var person1 = new Person("Richard", 20);
-console.log(Person.fullname());
-console.log(person1.totalAge);
-
-var student1 = new Student("Richard", 40, "Musime");
-console.log(Student.fullDetails());
-console.log(student1.studentAge);
-console.log(student1.studentLastName);
+var person2 = new Person("Thegym",20);
+console.log(person2.firstname("richard").totalAge(40).fullname());
